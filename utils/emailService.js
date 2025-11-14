70086291986aef8f80a497235f95955cb12035c9
raj-kumar-wezo <raj@wezo.co>
2025-11-14 22:53:19 +0530

    initial commit

--- utils/emailService.js
+++ utils/emailService.js
@@ -7,8 +7,6 @@ const createTransporter = () => {
 
   return nodemailer.createTransport({
     service: "gmail",
-    port: 587,
-    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
